86f56f8edb6f61485720a111548bfc649f6e8b25
JiYin <yinji921@gmail.com>
2021-07-03 12:11:59 -0400

    added the test case run_envrionment.py

---
+++ scripts/run_environment.py
@@ -0,0 +1,23 @@
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
+from robot_planning.environment.environment import Environment
+import numpy as np
+
+
+def main():
+    config_path = "configs/run_environment.cfg"
+    config_data = ConfigParser.ConfigParser()
+    config_data.read(config_path)
+    environment = Environment()
+    environment.initialize_from_config(config_data, 'environment')
+
+    actions = np.asarray([[0, 1], [0, -1]]).T
+    for i in range(100):
+        states, costs = environment.step(actions)
+        print(states, "    ", costs)
+
+
+if __name__ == '__main__':
+    main()
