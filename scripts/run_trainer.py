430e24a3d066fab82bc02eb309d3458cf8132db1
Scott Guan <yguan44@gatech.edu>
2021-08-27 12:23:44 -0400

    MADDPG trainer works with point dynamics

--- scripts/run_trainer.py
+++ scripts/run_trainer.py
@@ -5,23 +5,26 @@ except ImportError:
 
 from robot_planning.trainers.trainer import Trainer
 
+EVALUATE_ONLY = True
 
 def main():
     config_path = "configs/run_trainer.cfg"
     config_data = ConfigParser.ConfigParser()
     config_data.read(config_path)
-    trainer = Trainer()
-    trainer.initialize_from_config(config_data=config_data, section_name='trainer')
-    print('trainer initialized!')
 
-    # train
-    trainer.train()
+    if not EVALUATE_ONLY:
+        trainer = Trainer()
+        trainer.initialize_from_config(config_data=config_data, section_name='trainer')
+        print('trainer initialized!')
+
+        # train
+        trainer.train()
 
     # evaluate
     evaluator = Trainer()
     evaluator.initialize_from_config(config_data=config_data, section_name='trainer')
     evaluator.load_model(episode=1999, training=False)
-    evaluator.evaluate(n_eval_episodes=50, eval_episode_length=50)
+    evaluator.evaluate(n_eval_episodes=50, eval_episode_length=50, visualize=True)
 
 
     print('done!')
