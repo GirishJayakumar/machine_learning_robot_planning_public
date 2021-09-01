cad0615cab99b6c1856f479cae186e6089f149cf
Jacob Knaup <jacobk@gatech.edu>
2021-09-01 11:14:16 -0400

    fix indentation

--- scripts/run_batch_Autorally_CSSMPC.py
+++ scripts/run_batch_Autorally_CSSMPC.py
@@ -25,31 +25,31 @@ def run_batch_Autorally_CSSMPC(run_batch_Autorally_CSSMPC_config_path, batch_con
 
 
 def run_single_Autorally_CSSMPC(config_data, experiment_name):
-        agent1 = factory_from_config(robot_factory_base, config_data, 'agent1')
-        renderer1 = factory_from_config(renderer_factory_base, config_data, 'renderer1')
-        logger = factory_from_config(logger_factory_base, config_data, 'logger')
-        agent1.set_renderer(renderer=renderer1)
-        logger.set_agent(agent=agent1)
-        logger.set_experiment_name(experiment_name)
-        while logger.get_num_of_laps() < 1:
-            try:
-                state_next, cost = agent1.take_action_with_controller()
-            except:
-                logger.add_number_of_failure()
-                agent1.reset_state()
-                agent1.reset_time()
-                logger.shutdown()
-            logger.calculate_number_of_laps(state_next, dynamics=agent1.dynamics,
-                                            goal_checker=agent1.cost_evaluator.goal_checker)
-            logger.calculate_number_of_collisions(state_next, dynamics=agent1.dynamics,
-                                                  collision_checker=agent1.cost_evaluator.collision_checker)
-            logger.log()
-            renderer1.show()
-            renderer1.clear()
-            print("state: ", state_next)
-            print("number of laps: ", logger.get_num_of_laps(), "number of collisions: ",
-                  logger.get_num_of_collisions(), "number of controller failures: ", logger.get_num_of_failures())
-        logger.shutdown()
+    agent1 = factory_from_config(robot_factory_base, config_data, 'agent1')
+    renderer1 = factory_from_config(renderer_factory_base, config_data, 'renderer1')
+    logger = factory_from_config(logger_factory_base, config_data, 'logger')
+    agent1.set_renderer(renderer=renderer1)
+    logger.set_agent(agent=agent1)
+    logger.set_experiment_name(experiment_name)
+    while logger.get_num_of_laps() < 1:
+        try:
+            state_next, cost = agent1.take_action_with_controller()
+        except:
+            logger.add_number_of_failure()
+            agent1.reset_state()
+            agent1.reset_time()
+            logger.shutdown()
+        logger.calculate_number_of_laps(state_next, dynamics=agent1.dynamics,
+                                        goal_checker=agent1.cost_evaluator.goal_checker)
+        logger.calculate_number_of_collisions(state_next, dynamics=agent1.dynamics,
+                                              collision_checker=agent1.cost_evaluator.collision_checker)
+        logger.log()
+        renderer1.show()
+        renderer1.clear()
+        print("state: ", state_next)
+        print("number of laps: ", logger.get_num_of_laps(), "number of collisions: ",
+              logger.get_num_of_collisions(), "number of controller failures: ", logger.get_num_of_failures())
+    logger.shutdown()
 
 
 if __name__ == '__main__':
