689970b10251c6fa74196131a0222e3f086278e5
Jacob Knaup <jacobk@gatech.edu>
2021-10-28 16:50:12 -0400

    merge in remote

--- controllers/CSSMPC/CSSMPC.py
+++ controllers/CSSMPC/CSSMPC.py
@@ -27,6 +27,7 @@ class CSSMPC(MpcController):
         self.track_w = 0.0
         self.Q_bar = np.empty(())
         self.R_bar = np.empty(())
+        self.X_bar = np.empty(())
 
     def initialize_from_config(self, config_data, section_name):
         MpcController.initialize_from_config(self, config_data, section_name)
@@ -53,20 +54,25 @@ class CSSMPC(MpcController):
         self.track_w = float(config_data.get(section_name, 'track_w'))
         self.Q_bar = np.kron(np.eye(self.N, dtype=int), self.cost_evaluator.Q)
         self.R_bar = np.kron(np.eye(self.N, dtype=int), self.cost_evaluator.R)
+        self.X_bar = np.zeros((self.n*self.N, 1)).flatten()
         self.solver = cs_solver.CSSolver(self.n, self.m, self.l, self.N, self.u_range, self.slew_rate, (False, ),
-                                         mean_only=True, k_form=1, prob_lvl=self.prob_lvl, chance_const_N=self.N)
+                                         mean_only=False, k_form=1, prob_lvl=self.prob_lvl, chance_const_N=self.N)
 
     def plan(self, state_cur):
         state_cur = state_cur.reshape((-1, 1))
         e_psi, e_y, s = self.dynamics.track.localize(np.array((state_cur[-2, :], state_cur[-1, :])), state_cur[-3, :])
-        state_cur[5:, :] = np.vstack((e_psi, e_y, s))
+        state_cur_map = state_cur.copy()
+        state_cur_map[5:, :] = np.vstack((e_psi, e_y, s))
         us = self.initial_control_sequence.copy()
-        xs = self.roll_out(state_cur, us, self.dt)
+        xs = self.roll_out(state_cur_map, us, self.dt)
         A, B, d = self.dynamics_linearizer.linearize_dynamics(xs[:, :-1], us, dt=self.dt)
         A = A.reshape((self.n, self.n, self.N), order='F')
         B = B.reshape((self.n, self.m, self.N), order='F')
         d = d.reshape((self.n, 1, self.N), order='F')
-        D = np.zeros((self.n, self.l))
+        y = state_cur_map.flatten() - self.X_bar[0:self.n]
+        print('y:', y)
+        # D = np.zeros((self.n, self.l))
+        D = np.diag(y)
         D = np.tile(D.reshape((self.n, self.l, 1)), (1, 1, self.N))
         A, B, d, D = self.dynamics_linearizer.form_long_matrices_LTV(A, B, d, D)
         sigma_0 = np.zeros((self.n, self.n))
@@ -76,12 +82,23 @@ class CSSMPC(MpcController):
         V, K = self.solver.solve()
         K = K.reshape((self.m * self.N, self.n * self.N))
         us = V.reshape((self.m, self.N), order='F')
-        X_bar = np.dot(A, xs[:, 0]) + np.dot(B, V) + d.flatten()
+        sigma_y = np.dot(np.dot(A, sigma_0), A.T) + np.dot(D, D.T)
+        sigma_X = np.dot(np.dot(np.eye(self.n*self.N) + np.dot(B, K), sigma_y), (np.eye(self.n*self.N) + np.dot(B, K)).T)
+        self.X_bar = np.dot(A, xs[:, 0]) + np.dot(B, V) + d.flatten()
         u = V[:self.m]  # + np.dot(K[jj*m:(jj+1)*m, jj*n:(jj+1)*n], y)
         u = np.where(u > self.u_range[:, 1], self.u_range[:, 1], u)
         u = np.where(u < self.u_range[:, 0], self.u_range[:, 0], u)
-        xs = X_bar.reshape((self.n, self.N), order='F')
+        xs = self.X_bar.reshape((self.n, self.N), order='F')
         self.set_initial_control_sequence(us)
+
+        dynamics = self.get_dynamics()
+        optimal_trajectory = np.zeros((self.n, self.N))
+        optimal_trajectory[:, 0:1] = dynamics.propagate(state_cur, us[:, 0:0 + 1], self.dt, cartesian=True)
+        for ii in np.arange(1, self.N):
+            optimal_trajectory[:, ii:ii+1] = dynamics.propagate(optimal_trajectory[:, ii-1:ii], us[:, ii:ii+1], self.dt, cartesian=True)
+        if self.renderer is not None:
+            # self.renderer.render_trajectories(trajectories, **{'color': "b"})
+            self.renderer.render_trajectories([optimal_trajectory], **{'color': "r"})
         return u
 
     def set_initial_control_sequence(self, initial_control_sequence):
