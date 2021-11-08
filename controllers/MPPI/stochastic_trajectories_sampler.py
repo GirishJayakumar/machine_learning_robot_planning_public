2b5bb73bca24b26f5fb8768e21b2d131456ed2ec
Ji Yin <yinji921@gmail.com>
2021-11-08 10:50:35 -0500

    fixed all types of stochastic trajectories sampler for Autorally MPPI. The only stochastic_trajectories_sampler not workingh for the general MPPI is the MPPI_stochastic_trajectories_sampler(need to fix the vectorized collision checker and cost_evaluator)

--- controllers/MPPI/stochastic_trajectories_sampler.py
+++ controllers/MPPI/stochastic_trajectories_sampler.py
@@ -38,14 +38,14 @@ class MPPIStochasticTrajectoriesSamplerSlowLoop(StochasticTrajectoriesSampler):
 
     def sample(self, state_cur, v, control_horizon, control_dim, dynamics, cost_evaluator):
         #  state_cur is the current state, v is the nominal control sequence
-        state_start = state_cur
+        state_start = state_cur.reshape((-1, 1))
         us = np.zeros((self.number_of_trajectories, control_dim, control_horizon-1))
         costs = np.zeros((self.number_of_trajectories, 1, 1))
         trajectories = []
         for i in range(self.number_of_trajectories):
             state_cur = state_start
             trajectory = np.zeros((dynamics.get_state_dim()[0], control_horizon))
-            trajectory[:, 0] = state_cur
+            trajectory[:, :1] = state_cur
             noises = self.noise_sampler.sample(control_dim, control_horizon - 1)
             if i > (1 - self.uncontrolled_trajectories_portion) * self.number_of_trajectories:
                 u = v + noises
@@ -53,9 +53,9 @@ class MPPIStochasticTrajectoriesSamplerSlowLoop(StochasticTrajectoriesSampler):
                 u = noises
             cost = 0
             for j in range(control_horizon-1):
-                cost += cost_evaluator.evaluate(state_cur, u[:, j], dynamics=dynamics)
-                state_cur = dynamics.propagate(state_cur, u[:, j])
-                trajectory[:, j+1] = state_cur
+                cost += cost_evaluator.evaluate(state_cur, u[:, j:j+1], dynamics=dynamics)
+                state_cur = dynamics.propagate(state_cur, u[:, j]).reshape((-1, 1))
+                trajectory[:, j+1:j+2] = state_cur
             cost += cost_evaluator.evaluate_terminal_cost(state_cur, dynamics=dynamics)
             trajectories.append(trajectory)
             us[i, :, :] = u
@@ -150,3 +150,38 @@ class MPPIParallelStochasticTrajectoriesSamplerMultiprocessing(StochasticTraject
             cost += cost_evaluator.evaluate_terminal_cost(state_cur.reshape((-1, 1)), dynamics=dynamics)
             results.put([trajectory, u, cost])
             noises_queue.task_done()
+
+
+class CCMPPIStochasticTrajectoriesSampler(StochasticTrajectoriesSampler):
+    def __init__(self, number_of_trajectories=None, uncontrolled_trajectories_portion=None, noise_sampler=None):
+        StochasticTrajectoriesSampler.__init__(self, number_of_trajectories, uncontrolled_trajectories_portion, noise_sampler)
+
+    def initialize_from_config(self, config_data, section_name):
+        StochasticTrajectoriesSampler.initialize_from_config(self, config_data, section_name)
+
+    def sample(self, state_cur, v, control_horizon, control_dim, dynamics, cost_evaluator):
+        #  state_cur is the current state, v is the nominal control sequence
+        state_start = state_cur.copy()
+        # us = np.zeros((self.number_of_trajectories, control_dim, control_horizon-1))
+        costs = np.zeros((self.number_of_trajectories, 1, 1))
+        # trajectories = []
+        state_cur = np.tile(state_start.reshape((-1, 1)), (1, self.number_of_trajectories))
+        trajectories = np.zeros((state_cur.shape[0], control_horizon, self.number_of_trajectories))
+        trajectories[:, 0, :] = state_cur
+        noises = self.noise_sampler.sample(control_dim, (control_horizon - 1) * self.number_of_trajectories)
+        noises = noises.reshape((control_dim, (control_horizon - 1), self.number_of_trajectories))
+        num_controlled_trajectories = int((1 - self.uncontrolled_trajectories_portion) * self.number_of_trajectories)
+        us = np.zeros((v.shape[0], v.shape[1], self.number_of_trajectories))
+        us[:, :, :num_controlled_trajectories] = np.expand_dims(v, axis=2)
+        us += noises
+        # cost = 0
+        for j in range(control_horizon-1):
+            costs += cost_evaluator.evaluate(state_cur, us[:, j, :], noises[:, j, :], dynamics=dynamics)
+            state_cur = dynamics.propagate(state_cur, us[:, j, :])
+            trajectories[:, j+1, :] = state_cur
+        costs += cost_evaluator.evaluate_terminal_cost(state_cur, dynamics=dynamics)
+        # trajectories.append(trajectory)
+        # us[i, :, :] = u
+        # costs[:, 0, 0] = cost
+        us = np.moveaxis(us, 2, 0)
+        return trajectories, us, costs
