e4f9f27db1f1593a1aff1f6dc07b1c8646dec41c
Girish Jayakumar <66281004+GirishJayakumar@users.noreply.github.com>
2023-10-31 17:41:55 -0400

    tuned hyperparameters bounds for gaussian process ML model and added logging for hyperparameters
    tuned hyperparameters bounds for gaussian process ML model and added logging for hyperparameters in simulated_robot class

--- environment/robots/simulated_robot.py
+++ environment/robots/simulated_robot.py
@@ -10,6 +10,9 @@ import copy
 from copy import deepcopy
 import ast
 import time
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, WhiteKernel, Matern, ExpSineSquared
+import time
 
 
 class SimulatedRobot(Robot):
@@ -28,6 +31,16 @@ class SimulatedRobot(Robot):
         self.renderer = renderer
         self.observer = observer
         self.robot_index = robot_index
+        self.opponent_inputs = np.zeros((1, 8))
+        self.opponent_outputs = np.zeros((1, 8))
+        noise_level = 0.01  # Initial noise level (this can be adjusted)
+        noise_level_bounds = (1e-10, 0.1)  # Bounds on the noise level (this can also be adjusted)
+        #hyperparameter tuning
+        self.kernel = C(constant_value_bounds=(1e-5, 1e6)) * (RBF(length_scale_bounds=(1e-5, 1e5)) + Matern(length_scale_bounds=(1e-5, 1e5)) + ExpSineSquared(length_scale_bounds=(1e-5, 1e5))) + WhiteKernel(noise_level_bounds=(1e-12, 1e2))
+        self.gp_model = GaussianProcessRegressor(kernel=self.kernel, alpha=0.0001, n_restarts_optimizer=10)
+        self.old_opp_state = None
+
+
 
     def initialize_from_config(self, config_data, section_name):
         Robot.initialize_from_config(self, config_data, section_name)
@@ -35,6 +48,7 @@ class SimulatedRobot(Robot):
         self.dynamics = factory_from_config(dynamics_factory_base, config_data, dynamics_section_name)
         self.state = np.asarray(ast.literal_eval(config_data.get(section_name, 'start_state')))
         self.start_state = self.state
+        self.cost_evaluator = config_data.get(section_name, 'cost_evaluator')
         self.data_type = config_data.get(section_name, 'data_type')
         if config_data.has_option(section_name, 'steps_per_action'):
             self.steps_per_action = config_data.getint(section_name, 'steps_per_action')
@@ -56,6 +70,21 @@ class SimulatedRobot(Robot):
             self.observer = factory_from_config(observer_factory_base, config_data, observer_section_name)
         if config_data.has_option(section_name, 'robot_index'):
             self.robot_index = int(config_data.get(section_name, 'robot_index'))
+        # Adjusted bounds
+        self.kernel = C(constant_value_bounds=(1e-6, 1e7)) * (RBF(length_scale_bounds=(1e-5, 1e9)) + Matern(length_scale_bounds=(1e-5, 1e9)) + ExpSineSquared(length_scale_bounds=(1e-5, 1e9), periodicity_bounds=(1e-5, 1e7))) + WhiteKernel(noise_level_bounds=(1e-13, 1e3))
+        self.gp_model = GaussianProcessRegressor(kernel=self.kernel, alpha=0.0001, n_restarts_optimizer=10)
+
+    def train_gp_model(self):
+        self.gp_model.fit(self.opponent_inputs, self.opponent_outputs)
+
+    def predict_opponent_state(self, opponent_state):
+        # Predict the next state of the opponent using the GP model
+        X = opponent_state.reshape(1, -1)
+        # X = np.repeat(opponent_state[None, :], len(opponent_state), axis=0)
+        # X[:, self.robot_index * self.dynamics.get_state_dim():self.robot_index * self.dynamics.get_state_dim() + self.dynamics.get_action_dim()] = self.actions
+        Y_pred = self.gp_model.predict(X)
+        opponent_state_pred = Y_pred.mean(axis=0)
+        return opponent_state_pred
 
     @property
     def delta_t(self):
@@ -66,6 +95,10 @@ class SimulatedRobot(Robot):
 
     def get_time(self):
         return self.steps * self.dynamics.get_delta_t()
+
+    def get_state_variance(self):
+        # Return the current covariance matrix of the robot's state
+        return self.covariance
 
     def get_state_dim(self):
         return self.dynamics.get_state_dim()
@@ -155,18 +188,23 @@ class SimulatedRobot(Robot):
             goal_color = self.cost_evaluator.goal_checker.get_goal_color()
             self.renderer.render_goal(goal=goal, color=goal_color)
 
-    def propagate_robot(self, action):
+    def propagate_robot(self, action, opp_state=None):
         assert isinstance(action, np.ndarray), 'simulated robot has numpy.ndarray type action!'
         if self.renderer is not None:
             if self.renderer.active:
                 self.render_all()
-        state_next = self.dynamics.propagate(self.state, action)
+        if opp_state is not None:
+            opp_state_pred, _ = self.predict_gp(opp_state)
+            state_next = self.dynamics.propagate(self.state, action, opp_state=opp_state_pred)
+        else:
+            state_next = self.dynamics.propagate(self.state, action)
         control_cost = 0
         self.set_state(state_next)
         self.steps += 1
         return state_next, control_cost
 
     def evaluate_state_action_pair_cost(self, state, action, state_next=None):
+        #not needed
         cost = self.cost_evaluator.evaluate(state.reshape((-1, 1)), action.reshape((-1, 1)), dynamics=self.dynamics)
         # print('Robot index {}, cost = {}'.format(self.robot_index, cost))
         return cost
@@ -203,15 +241,56 @@ class SimulatedRobot(Robot):
             self.steps += 1
         return state_next
 
-    def take_action_with_controller(self):
-        state_next = None
-        cost = 0
-        warm_start = True if self.steps == 0 else False
+    # don't know if this method is necessary
+    def predict_gp(self, obs):
+        #not needed
+        obs_2d = np.reshape(obs, (-1, 1))
+        return self.gp.predict(obs_2d, return_std=True)
 
-        action = self.controller.plan(state_cur=self.get_state(), warm_start=warm_start)
-        for _ in range(self.steps_per_action):
-            # print(self.get_state())
-            state_next, control_cost = self.propagate_robot(action)
-            cost += self.evaluate_state_action_pair_cost(state=state_next, action=action)
-        assert state_next is not None, 'invalid state!'
+    def update_training_data(self, opp_state):
+        # If no old state is stored, update it and return
+        if self.old_opp_state is not None:
+            self.opponent_inputs = np.vstack((self.opponent_inputs, self.old_opp_state))
+            self.opponent_outputs = np.vstack((self.opponent_outputs, opp_state))
+        self.old_opp_state = opp_state
+
+    def take_action_with_controller(self, opp_state):
+        if self.renderer is not None and self.renderer.active:
+            self.render_all()
+        state_cur = self.get_state()
+
+        # Predict opponent's state using GP model
+        if self.gp_model is not None:
+            # Add new state to training data
+            self.update_training_data(opp_state)
+
+            # Train GP model
+            start_time = time.time()
+            self.train_gp_model()
+
+            # Predict the opponent's state
+            opp_state_pred = self.predict_opponent_state(opp_state)
+            end_time = time.time()
+
+            # Print debug info
+            time_to_train_and_predict = end_time - start_time
+            print(f"Time to train and predict: {time_to_train_and_predict:.4f}")
+            for hp in self.kernel.hyperparameters:
+                print('gp', hp)
+            params = self.gp_model.kernel_
+            print(params)
+
+        # Plan control action
+        action = self.controller.plan(state_cur=state_cur, opp_state=opp_state_pred)
+
+        # Apply control action to propagate robot state
+        state_next = self.dynamics.propagate(state_cur, action)
+
+        # Evaluate cost
+        cost = self.cost_evaluator.evaluate(state_cur, action, state_next)
+
+        # Update robot state
+        self.set_state(state_next)
+        self.steps += 1
+
         return state_next, cost
